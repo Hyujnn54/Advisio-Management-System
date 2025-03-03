7303d6ce432472d622620583c4fbe1374c3c3b7a
Hyujnn <140203430+Hyujnn54@users.noreply.github.com>
2025-03-03 14:41:54 +0000

    up

--- build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/ui_mainwindow.h
+++ build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -462,7 +462,7 @@ public:
 
         retranslateUi(MainWindow);
 
-        tabWidget->setCurrentIndex(1);
+        tabWidget->setCurrentIndex(2);
 
 
         QMetaObject::connectSlotsByName(MainWindow);
